9bc40d1ed41f86f624a80a0869e7eda2d8452a00
renan <renan@gmail.com>
2025-06-12 21:21:59 -0300

    Adding classes genereted from staruml

---
+++ code/player.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Entity import Entity
+
+
+class Player(Entity):
+    def __init__(self):
+        pass
+
+    def Operation1(self, ):
+        pass
+
+    def Operation2(self, ):
+        pass
+
+    def move(self, ):
+        pass
